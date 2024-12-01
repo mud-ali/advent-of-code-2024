9e053d7832641f9f2c38dd7a751cc4cb401b97a1
mudasir <96320211+mud-ali@users.noreply.github.com>
2024-11-30 20:34:40 -0500

    create new day script and run for day 01

---
+++ new.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+# Ask for the day number
+echo "What day is it? "
+read day
+
+# Pad the day number to two digits
+day_padded=$(printf "%02d" $day)
+
+# Create the new folder
+mkdir "day$day_padded"
+cd "day$day_padded"
+touch main.py
+touch input.txt
+touch test.txt
